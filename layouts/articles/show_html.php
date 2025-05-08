2fb1d15ede5a8707b3e51696be59efdb3c04e21d
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-08 17:19:00 +0100

    F-ormulaire de commentaire

--- layouts/articles/show_html.php
+++ layouts/articles/show_html.php
@@ -25,7 +25,7 @@
 
 <?php if (isset($_SESSION['auth'])) : ?>
 
-  <!-- Formulaire de commentaire -->
+  <!-- F-ormulaire de commentaire -->
   <form action="save-comment.php" method="POST" class="comment-form">
     <h3 class="comment-form-heading">Vous voulez réagir ? N'hésitez pas !</h3><br>
 
