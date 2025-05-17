1255de5fd3d97a5d5df7fffe4819d2b91cd22a8d
ROSTAND LELE <rostoinfocus@gmail.com>
2025-05-17 10:34:12 +0100

    erre

--- logout.php
+++ logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-session_unset(); // Détruire toutes les variables de session
+session_unset(); // -Détruire toutes les variables de session
 session_destroy(); // Détruire la session
 header("Location: index.php"); // Rediriger vers la page de connexion
 exit(); // Terminer le script
